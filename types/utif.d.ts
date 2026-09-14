d8ef45a2fc7b849b097425faa5c49884de86b7e3
Kapil <106533381+Kapil8009@users.noreply.github.com>
2026-09-14 11:47:04 +0530

    Add TypeScript definitions for 'utif' module

---
+++ types/utif.d.ts
@@ -0,0 +1,15 @@
+declare module 'utif' {
+  export type Ifd = {
+    width: number;
+    height: number;
+    [key: string]: unknown;
+  };
+
+  const UTIF: {
+    decode(buffer: ArrayBuffer): Ifd[];
+    decodeImage(buffer: ArrayBuffer, ifd: Ifd): void;
+    toRGBA8(ifd: Ifd): Uint8Array;
+  };
+
+  export default UTIF;
+}
